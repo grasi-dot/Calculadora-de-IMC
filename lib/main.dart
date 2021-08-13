8c374767b40083d345b1902cc419bfd1255b7da0
Grasiela Eduarda Rodrigues Correa <grasiela.correa@C0109.local>
2021-08-12 22:48:26 -0300

    escopo pronto

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,42 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  @override
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+
+  String _infoText = "Informe seus Dados";
+
+  void _resetFields() {
+    weightController.text = "";
+    heightController.text = "";
+    setState(() {
+      _infoText = "Informe seus Dados";
+    });
+  }
+
+  void _calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+
+      if (imc < 18.6) {
+        _infoText = "Abaixo do peso {${imc.toStringAsPrecision(4)}}";
+      } else if (imc >= 20 && imc < 24.9) {
+        _infoText = "Peso ideal (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Levemente acima do peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obesidade grau I (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = "Obesidade grau II (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 39.9) {
+        _infoText = "Obesidade grau III (${imc.toStringAsPrecision(4)})";
+      }
+    });
+  }
+
+  @override //estou sobrescrevendo o que esta abaixo
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -22,24 +57,56 @@ class _HomeState extends State<Home> {
         actions: [
           IconButton(
             icon: Icon(Icons.refresh),
-            onPressed: () {},
+            onPressed: _resetFields,
           )
         ],
       ),
       backgroundColor: Colors.pink[50],
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Icon(Icons.person_outline, size: 120.0, color: Colors.purple[200]),
-          TextField(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Icon(Icons.person_outline, size: 120.0, color: Colors.purple[200]),
+            TextField(
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
                 labelText: "Peso (kg)",
                 labelStyle: TextStyle(color: Colors.purple[100]),
               ),
               textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.purple[100], fontSize: 25.0)),
-        ],
+              style: TextStyle(color: Colors.purple[100], fontSize: 25.0),
+              controller: weightController,
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Altura (cm)",
+                labelStyle: TextStyle(color: Colors.purple[100]),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.purple[100], fontSize: 25.0),
+              controller: heightController,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: Container(
+                height: 50.0,
+                child: RaisedButton(
+                  onPressed: _calculate,
+                  child: Text("Calcular",
+                      style: TextStyle(color: Colors.white, fontSize: 25.0)),
+                  color: Colors.purple[200],
+                ),
+              ),
+            ),
+            Text(
+              _infoText,
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.purple[100], fontSize: 25.0),
+            ),
+          ],
+        ),
       ),
     );
   }
