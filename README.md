68f8b19f690595b0b4d954b29fd59dad6ef17ccb
Grasiela Eduarda Rodrigues Correa <grasiela.correa@C0109.local>
2021-08-13 19:27:59 -0300

    Projeto finalizado

--- README.md
+++ README.md
@@ -1,16 +1,25 @@
-# imc
+# Calculadora de IMC
 
-A new Flutter project.
+O IMC  é a sigla para Índice de Massa Corporal,que é um cálculo que serve para avaliar se a pessoa está dentro do seu peso ideal em relação à altura.
 
-## Getting Started
+Classificação	IMC	O que pode acontecer
+Muito abaixo do peso	16 a 16,9 kg/m2	
+Abaixo do peso	17 a 18,4 kg/m2
+Peso normal	18,5 a 24,9 kg/m2	
+Acima do peso	25 a 29,9 kg/m2	
+Obesidade Grau I	30 a 34,9 kg/m2	
+Obesidade Grau II	35 a 40 kg/m2
+Obesidade Grau III	maior que 40 kg/m2
 
-This project is a starting point for a Flutter application.
+## Ferramentas
 
-A few resources to get you started if this is your first Flutter project:
+- Vs Code
+- Simulator
+- Framework Flutter
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+## Fontes
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+- https://www.tuasaude.com/imc/
+
+- Curso Udemy https://www.udemy.com/course/curso-completo-flutter-app-android-ios/
+
--- lib/main.dart
+++ lib/main.dart
@@ -15,13 +15,19 @@ class _HomeState extends State<Home> {
   TextEditingController weightController = TextEditingController();
   TextEditingController heightController = TextEditingController();
 
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   String _infoText = "Informe seus Dados";
 
   void _resetFields() {
+    _formKey.currentState.reset();
+
     weightController.text = "";
+
     heightController.text = "";
+
     setState(() {
-      _infoText = "Informe seus Dados";
+      _infoText = "Informe seus dados!";
     });
   }
 
@@ -64,48 +70,68 @@ class _HomeState extends State<Home> {
       backgroundColor: Colors.pink[50],
       body: SingleChildScrollView(
         padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Icon(Icons.person_outline, size: 120.0, color: Colors.purple[200]),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Peso (kg)",
-                labelStyle: TextStyle(color: Colors.purple[100]),
-              ),
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.purple[100], fontSize: 25.0),
-              controller: weightController,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Altura (cm)",
-                labelStyle: TextStyle(color: Colors.purple[100]),
-              ),
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.purple[100], fontSize: 25.0),
-              controller: heightController,
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-              child: Container(
-                height: 50.0,
-                child: RaisedButton(
-                  onPressed: _calculate,
-                  child: Text("Calcular",
-                      style: TextStyle(color: Colors.white, fontSize: 25.0)),
-                  color: Colors.purple[200],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Icon(Icons.person_outline,
+                  size: 120.0, color: Colors.purple[200]),
+              TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: "Peso (kg)",
+                    labelStyle: TextStyle(color: Colors.purple[100]),
+                  ),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.purple[100], fontSize: 25.0),
+                  controller: weightController,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return "Insira seu peso!";
+                    } else {
+                      return null;
+                    }
+                  }),
+              TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: "Altura (cm)",
+                    labelStyle: TextStyle(color: Colors.purple[100]),
+                  ),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.purple[100], fontSize: 25.0),
+                  controller: heightController,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return "Insira sua Altura!";
+                    } else {
+                      return null;
+                    }
+                  }),
+              Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                child: Container(
+                  height: 50.0,
+                  child: RaisedButton(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        _calculate();
+                      }
+                    },
+                    child: Text("Calcular",
+                        style: TextStyle(color: Colors.white, fontSize: 25.0)),
+                    color: Colors.purple[200],
+                  ),
                 ),
               ),
-            ),
-            Text(
-              _infoText,
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.purple[100], fontSize: 25.0),
-            ),
-          ],
+              Text(
+                _infoText,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.purple[100], fontSize: 25.0),
+              ),
+            ],
+          ),
         ),
       ),
     );
